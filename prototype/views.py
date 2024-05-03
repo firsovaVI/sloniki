d79bced80e65210df1f26987ddd70179c49f199f
AnnaK <69359059+AnsKaz-yu@users.noreply.github.com>
2024-05-03 13:54:58 +0300

    Добавлены эндпоинты для страницы аккаунта и классного журнала

--- prototype/views.py
+++ prototype/views.py
@@ -68,3 +68,76 @@ def account(request):
     message += f"<h1>First Name: {user.first_name} </h1>"
     message += f"<h1>Last Name: {user.last_name} </h1>"
     return HttpResponse(message)
+
+def user_detail_api(request):
+    """Endpoint, который возвращает личную информацию об авторизированном пользователе"""
+    id = request.session["session_id"]
+    user = User.objects.get(pk=id)
+    
+    data = {
+        'user_id': user.id,
+        'username': user.username,
+        'email': user.email,
+        'first_name': user.first_name,
+        'last_name': user.last_name,
+        'group': user.group
+        }
+    return JsonResponse(data)
+
+def all_students_api(request):
+    """Endpoint, который возвращает список учеников в данном классе"""
+    students = User.objects.filter(group='ST')
+    
+    data = []
+    for student in students:
+        solved_problems_list = []
+        for p in student.solved_problems.all():
+            solved_problems_list.append(p.pk)
+        
+            st = {
+                'first_name': student.first_name,
+                'last_name': student.last_name,
+                'solved_problems': solved_problems_list
+                }
+            data.append(st)
+    
+    return JsonResponse({'students_list': data})
+
+def all_homeworks_api(request):
+    """Endpoint, который возвращает список всех проверочных работ для класса"""
+    id = request.session["session_id"]
+    user = User.objects.get(pk=id)
+    
+    homeworks = Homework.objects.all()
+    data = []
+    for homework in homeworks:
+        hw = {
+            'homework_id': homework.pk,
+            'headline': homework.headline,
+            'deadline': homework.deadline,
+            'url': reverse('homework', kwargs={'homework_id': homework.id})
+            }
+        if user.group == 'TH':
+            hw['url'] += 'gradebook/'
+        data.append(hw)
+    return JsonResponse({'homeworks_list': data})
+
+def homework_detail_api(request, id):
+    """Endpoint, который возвращает информацию о проверочной работе по id"""
+    homework = Homework.objects.get(pk=id)
+    problems_list = []
+    for problem in homework.problems.all():
+        url = reverse('detail', kwargs={'problem_id': problem.id})
+        p = {
+            'problem_id': problem.id,
+            'headline': problem.headline,
+            'url': url
+            }
+        problems_list.append(p)
+    
+    data = {
+        'headline': homework.headline,
+        'deadline': homework.deadline,
+        'problems': problems_list
+        }
+    return JsonResponse(data)
